5125afdcb4876ba4ec070c652e73c8f2f82b918d
SKDK <muthukumaran.shanmugam@directlinegroup.co.uk>
2021-04-22 13:09:29 +0100

    Additional Function

--- src/main/java/sample/checking.java
+++ src/main/java/sample/checking.java
@@ -5,4 +5,8 @@ public class checking {
     public static void display(String param){
         System.out.println("Your parameter is :" + param);
     }
+
+    public static void showAnother(String param){
+        System.out.println("Second method is :" + param);
+    }
 }
